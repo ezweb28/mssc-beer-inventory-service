a79891b0bf7a62b8cf3765c5fdd2d48631c3482e
Sunpil Kim <ezweb28@gmail.com>
2023-02-22 01:23:43 -0500

    Setup local profile

--- src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
+++ src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
@@ -22,6 +22,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -45,6 +46,7 @@ public class BeerInventory extends BaseEntity{
     }
 
     @Type(type = "org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID beerId;
     private String upc;
     private Integer quantityOnHand = 0;
